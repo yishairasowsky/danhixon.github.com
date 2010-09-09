113b73aa72dfea92457559b0a3d6b9c5db0ab205
Dan Hixon <danhixon@gmail.com>
2010-09-09 09:52:17 -0700

    publishing image comparisons jquery plugin

--- javascripts/jquery.compare.js
+++ javascripts/jquery.compare.js
@@ -1,19 +1,20 @@
 (function($){ 
-	
 	$.fn.compare = function(options)
 	{
 		$(this).each(function(index, one){
-			
 			var first = $(one).find('img:first');
 			var other = $(one).find('img:last');
-			other.hide();
+
+			//Hide all but the first:
+			$(one).find('img').hide();
+			first.show();
 
 			$(one).mouseover(function(){
 				first.hide();
 				other.show();
 			}).mouseout(function(){
-				first.show();
 				other.hide();
+				first.show();
 			});
 		});
 		
